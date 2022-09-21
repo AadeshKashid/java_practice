92aaffda19064b9299539df4336019c93dacd21e
Aadeshkashid <aadeshkashid0911@gmail.com>
2022-09-21 19:21:20 +0530

    SwapValue program

---
+++ Swap_Value/swapValue.java
@@ -0,0 +1,14 @@
+class SwapValue{
+    public static void main(String arg[])
+    {
+        int a,b;
+        a=20;
+        b=30;
+        System.out.println("The value are : a, b = "+a+", "+ + b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("the swapping value is:a,b="+a+","+ + b);
+    }
+}
+
