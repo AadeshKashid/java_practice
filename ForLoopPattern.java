96c0871a5e727b94cfd69db7aeed70997094ba75
AadeshKashid <113301287+AadeshKashid@users.noreply.github.com>
2022-10-13 21:04:09 +0530

    For Loop Program
    Pattern Printing

---
+++ ForLoopPattern.java
@@ -0,0 +1,34 @@
+package com.test;
+
+public class ForLoopPattern {
+
+	public static void main(String[] args) {
+		
+		int i=1;
+		for(i=1; i<=5;i++) 
+		{
+			int j=1;
+			for(j=1;j<=i;j++) 
+			{
+			
+			
+			System.out.print("*");
+			
+		}
+			System.out.println("");
+	}
+		for(i=5; i>=1;i--) 
+		{
+			int j=1;
+			for(j=1;j<=i;j++) 
+			{
+			
+			
+			System.out.print("*");
+			
+		}
+			System.out.println("");
+	}
+		
+}
+}
